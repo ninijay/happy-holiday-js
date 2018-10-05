9876606ddad0ff70e167d233e3933cf4f5384bc3
Mattia Ninviaggi <mattia.ninivaggi@compass-security.com>
2018-10-05 14:47:24 +0200

    added imgs

--- js/main.js
+++ js/main.js
@@ -6,23 +6,26 @@ $(document).ready(function(){
         $("#pics").css("background-image", "url(\"img/"+url+"\"");
     }
 
-    if(n >= 11)
+    if(n >= 11 && n < 13)
     {
         setBG("christmas.jpg");
     }
-    if(n == 10)
+    else if(n == 10)
     {
         setBG("spooky.jpg");
     }
-    if(n == 1)
+    else if(n == 1)
     {
         setBG("newyear.jpg");
     }
-    if(n == 4)
+    else if(n == 4)
     {
         setBG("eastern.jpg");
     }
-    if(n >= 6 && n <= 8){
+    else if(n >= 6 && n <= 8){
         setBG("summer.jpg");
+    }
+    else{
+        setBG("default.gif");
     }
 });
